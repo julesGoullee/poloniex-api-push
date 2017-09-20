67475d7e987cd8dbd47b60154d87b4db1ae5a343
jules goullée <julesgoullee@gmail.com>
2017-09-20 17:18:07 +0200

    first step

---
+++ README.md
@@ -0,0 +1,73 @@
+Poloniex-api-push
+=================
+
+Client to use poloniex api push.
+I'm writing after read official [official api](https://poloniex.com/support/api/) documentation autobahn doesnt work.
+After reverse poloniex web app, it seem to be not using autobahn and wamp protocole but pur websocket implementation, I created package to wrap this.
+It usable for real time data data, more fresh compare to http api.
+
+
+Features:
+- watch multiple pairs in same time
+- orderbooks (buy/sell, insert/delete)
+- trades
+- reconnected automatically
+
+# Install
+
+    npm install --save poloniex-api-push
+
+# Usages:
+
+```js
+const PoloniexApiPush = require('poloniex-api-push');
+const poloPush = new PoloniexApiPush();
+
+// You need to call before every methods ensure connection is establish
+
+poloPush.init().then(() => {
+
+    //call some methods
+
+});
+```
+
+# Methods:
+
+It's an eventListener, on can add/remove listener has well [https://nodejs.org/api/events.html](https://nodejs.org/api/events.html)
+
+Watch pair:
+```js
+poloPush.subscribe('BTC_ETH');
+```
+
+Unwatch pair:
+```js
+poloPush.unSubscribe('BTC_ETH');
+```
+
+Orderbooks:
+```js
+poloPush.on('BTC_ETH-orderbook-bids', (orderbookBid) => {
+
+    console.log('BTC_ETH-orderbook-bids', orderbookBid.rate, orderbookBid.amount);
+
+});
+
+poloPush.on('BTC_ETH-orderbook-asks', (orderbookAsk) => {
+
+    console.log('BTC_ETH-orderbook-asks', orderbookAsk.rate, orderbookAsk.amount);
+
+});
+```
+
+If amount is set to '0.00000000' this rate is removed, see poloniex offical api.
+
+Trades:
+```js
+poloPush.on('BTC_ETH-trade', (trade) => {
+
+    console.log('BTC_ETH-trade', trade.type, trade.rate, trade.amount);
+
+  });
+```
---
+++ index.js
@@ -0,0 +1,153 @@
+const WebSocket = require('ws');
+const EventEmitter = require('events').EventEmitter;
+const markets = require('./markets.json');
+
+class PoloniexApiPush extends EventEmitter {
+
+  constructor(){
+
+    super();
+    this.urlPath = 'api2.poloniex.com';
+    this.ws = null;
+    this.subscriptions = [];
+
+  }
+
+  init(){
+
+    this._ws = new WebSocket(`wss:${this.urlPath}`);
+    let resolver = null;
+    const p = new Promise(resolve => resolver = resolve);
+
+    this._ws.on('open', () => {
+
+      this.onOpen(resolver);
+
+    });
+
+    this._ws.on('close', () => this.resetWS() );
+
+    return p;
+
+  }
+
+  resetWS(){
+
+    this.init().then(() => {
+     
+      this.subscriptions.forEach(channel => this.subscribe(channel) );
+     
+    }).catch(err => console.error(err) );
+
+  }
+  
+  onOpen(cb){
+
+    this.heartbeat = setInterval( () => {
+      
+      try{
+
+        this._ws.send('.');
+        
+      } catch(err){
+
+        clearTimeout(this.heartbeat);
+        this.resetWS();
+        
+      }
+      
+    }, 60000);
+    
+    this.dispatchMessages();
+    cb();
+
+  }
+
+  dispatchMessages(){
+
+    this._ws.on('message',  (mess) => {
+
+      if(mess.length === 0){
+
+        return;
+
+      }
+
+      const data = JSON.parse(mess);
+      const seq = data[1];
+
+      if(typeof seq === 'number'){
+
+        const market = markets.byID[data[0]];
+
+        data[2].forEach( (entry) => {
+
+          switch(entry[0]){
+
+            case 'i':
+
+              Object.keys(entry[1].orderBook[0]).forEach(rate => this.emit(`${market.currencyPair}-orderbook-asks`, {rate, amount: entry[1].orderBook[0][rate]}) );
+              Object.keys(entry[1].orderBook[1]).forEach(rate => this.emit(`${market.currencyPair}-orderbook-bids`, {rate, amount: entry[1].orderBook[1][rate]}) );
+              break;
+
+            case 'o':
+
+              this.emit(`${market.currencyPair}-orderbook-${entry[1] === 1 ? 'bids' : 'asks'}`, {
+                rate: entry[2],
+                amount: entry[3]
+              });
+              break;
+
+            case 't':
+
+              this.emit(`${market.currencyPair}-trade`, {
+                type: entry[1] === 1 ? 'buy' : 'sell',
+                rate: entry[3],
+                amount: entry[4]
+              });
+              break;
+
+            default:
+              console.error(`Unknown message: ${data}`);
+              break;
+          }
+
+        });
+
+      }
+
+    });
+
+  }
+
+
+  subscribe(pair){
+
+    this.subscriptions.push(pair);
+    this.send('subscribe', pair);
+
+  }
+
+  unSubscribe(pair){
+
+    this.subscriptions = this.subscriptions.filter(channel => channel !== pair);
+
+    this.send('unsubscribe', pair);
+
+  }
+
+  send(cmd, channel){
+
+    if(this._ws instanceof WebSocket && this._ws.readyState > 0){
+
+      return this._ws.send(JSON.stringify({command: cmd, channel }) );
+
+    }
+
+    throw new Error('Not connected');
+
+  }
+
+}
+
+module.exports = PoloniexApiPush;
